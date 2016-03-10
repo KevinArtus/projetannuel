66f36f954af3f6ffcbf1828540f24e0717ceca9d
Kevin Artus <kevinar9@hotmail.fr>
2016-03-10 11:35:44 +0100

    Correction install, correct

--- web/app_dev.php
+++ web/app_dev.php
@@ -2,7 +2,6 @@
 
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Debug\Debug;
-
 // If you don't want to setup permissions the proper way, just uncomment the following PHP line
 // read http://symfony.com/doc/current/book/installation.html#checking-symfony-application-configuration-and-setup
 // for more information
@@ -22,7 +21,9 @@ use Symfony\Component\Debug\Debug;
  * @var Composer\Autoload\ClassLoader $loader
  */
 $loader = require __DIR__.'/../app/autoload.php';
+
 Debug::enable();
+require __DIR__ . '/../app/AppKernel.php';
 
 $kernel = new AppKernel('dev', true);
 $kernel->loadClassCache();
